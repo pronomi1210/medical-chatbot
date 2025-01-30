5b2f069c7ff886a3dd5e31d9c4bb2e4b68ec53eb
pronomi1210 <136000453+pronomi1210@users.noreply.github.com>
2025-01-31 02:07:12 +0530

    frontend and prompt

---
+++ End-to-end-Medical-Chatbot-using-Llama2/src/prompt.py
@@ -0,0 +1,12 @@
+
+
+prompt_template="""
+Use the following pieces of information to answer the user's question.
+This information is not known by me. Sorry for the inconvenience. (Refrence: I only answer what is present in the textbook)
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
