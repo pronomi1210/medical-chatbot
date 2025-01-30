5b987061af7f8665ac6226af50fbfedb74d6643f
pronomi1210 <136000453+pronomi1210@users.noreply.github.com>
2025-01-31 02:05:30 +0530

    python files

---
+++ End-to-end-Medical-Chatbot-using-Llama2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Pronomi Roy',
+    author_email= 'pr1885@srmist.edu.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
